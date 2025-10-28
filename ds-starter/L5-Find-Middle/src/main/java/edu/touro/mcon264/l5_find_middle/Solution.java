fe95e9b83cf0775de3ae77c466ff18eb495ed097
Cweiss15 <cweiss15@student.touro.edu>
2025-10-28 19:44:05 -0400

    Solution, unit tests, and README explanation completed

--- ds-starter/L5-Find-Middle/src/main/java/edu/touro/mcon264/l5_find_middle/Solution.java
+++ ds-starter/L5-Find-Middle/src/main/java/edu/touro/mcon264/l5_find_middle/Solution.java
@@ -1,5 +1,69 @@
 package edu.touro.mcon264.l5_find_middle;
 
-public class Solution {
-    // TODO: Implement solution for: Find Middle of a List
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
+
+class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getMiddle() {
+        if (head == null) return null;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        Node middle = list.getMiddle();
+        System.out.println("The middle of the given list is: " + middle.data);
+    }
+}
+
+
--- ds-starter/L5-Find-Middle/src/test/java/edu/touro/mcon264/l5_find_middle/SolutionTest.java
+++ ds-starter/L5-Find-Middle/src/test/java/edu/touro/mcon264/l5_find_middle/SolutionTest.java
@@ -1,10 +1,50 @@
 package edu.touro.mcon264.l5_find_middle;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class SolutionTest {
+
     @Test
-    void testPlaceholder() {
-        // TODO: Add tests for Find Middle of a List
+    @DisplayName("Middle of odd list")
+    public void testMiddleOddLinkedList() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Node middle = list.getMiddle();
+        assertEquals(2, middle.getData());
+    }
+
+    @Test
+    @DisplayName("Middle of equal list returns first of 2 center nodes")
+    public void testMiddleEvenLinkedList2() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Node middle = list.getMiddle();
+        assertEquals(2, middle.getData());
+    }
+
+    @Test
+    @DisplayName("Empty list middle returns null")
+    public void testMiddleEmptyLinkedListIsNull() {
+        LinkedList list = new LinkedList();
+        Node middle = list.getMiddle();
+        assertNull(middle);
+    }
+
+    @Test
+    @DisplayName("Middle of single item list returns that item")
+    public void TestMiddleOfSingleItemListIsThatItem() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        Node middle = list.getMiddle();
+        assertEquals(1, middle.getData());
     }
 }
